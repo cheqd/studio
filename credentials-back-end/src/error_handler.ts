12d855ea209cbda75051cdd1a7a1b34379f6f0c1
Tasos <50984242+Eengineer1@users.noreply.github.com>
2022-04-16 23:44:33 +0200

    Added safe catch-all

--- credentials-back-end/src/error_handler.ts
+++ credentials-back-end/src/error_handler.ts
@@ -2,6 +2,4 @@ import { ErrorResponse } from './api/types'
 
 const error_handler = ( error: ErrorResponse ): Response => { return new Response( error.message || 'Internal Server Error', { status: error.status || 500 } ) }
 
-export default {
-    error_handler: error_handler
-}
+export default error_handler
--- credentials-back-end/src/router.ts
+++ credentials-back-end/src/router.ts
@@ -1,12 +1,13 @@
 import { Router } from 'itty-router'
 import auth_router from './api/routes/authentication'
 import credentials_router from './api/routes/credentials'
+import error_handler from './error_handler'
 
 const router = Router({ base: '/api' })
 
 router
     .all( '/authentication/*', auth_router.handle)
     .all( '/credentials/*', credentials_router.handle)
-    .all( '*',  )
+    .all( '*', error_handler )
 
 export default router
