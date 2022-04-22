191d383ab2fb34aba3f1ffee429a5e79f6a4b03d
Tasos <50984242+Eengineer1@users.noreply.github.com>
2022-04-22 13:13:03 +0200

    Added basic module settings & credential / identity issuance

--- credentials-back-end/src/api/controllers/identity.ts
+++ credentials-back-end/src/api/controllers/identity.ts
@@ -4,6 +4,7 @@ import {
     IResolver,
     IDataStore,
     IKeyManager,
+    IIdentifier,
     TAgent,
 } from '@veramo/core'
 import { DIDManager, MemoryDIDStore } from '@veramo/did-manager'
@@ -20,14 +21,13 @@ import { getResolver as WebDIDResolver } from 'web-did-resolver'
 import { KMS_SECRET_KEY, INFURA_PROJECT_ID, VC_SUBJECT, ISSUER_ID, VC_CONTEXT, VC_TYPE, HEADERS, VC_PROOF_FORMAT } from '../constants'
 import { CredentialPayload, CredentialSubject } from '../types'
 
-import { Identity } from './identity'
-
-export class Credentials {
+export class Identity {
 
     agent: TAgent<any> | null | undefined
 
-    constructor(agent?: TAgent<any>) {
+    constructor(agent?: TAgent<any>, mode?: string) {
         this.agent = agent
+        if( mode === 'demo' ) return
         if( !agent ) this.init_agent()
     }
 
@@ -69,48 +69,16 @@ export class Credentials {
         })
     }
 
-    async issue_credentials(request: Request): Promise<Response> {
-        if( !this.agent ) throw new Error('No initialised agent found.')
+    async create_demo_id(request: Request, agent: TAgent<any>): Promise<IIdentifier> {
+        if( !this.agent && !agent ) throw new Error('No initialised agent found.')
 
-        const issuer_id = await (new Identity(
-            this.agent,
-            'demo'
-        )).create_demo_id(
-            request,
-            this.agent
+        if( agent ) this.agent = agent
+
+        const identity = await agent.execute(
+            'didManagerCreate',
+            {}
         )
 
-        const credential_subject: CredentialSubject = {
-            id: VC_SUBJECT,
-            type: undefined
-        }
-
-        const credential: CredentialPayload = {
-            issuer: { id: issuer_id.did },
-            '@context': VC_CONTEXT,
-            type: [ VC_TYPE ],
-            issuanceDate: new Date().toISOString(),
-            credentialSubject: credential_subject
-        }
-
-        const verifiable_credential = await this.agent.execute(
-            'createVerifiableCredential',
-            {
-                save: false,
-                credential,
-                proofFormat: VC_PROOF_FORMAT
-            }
-        )
-
-        return new Response(
-            JSON.stringify(
-                verifiable_credential,
-                null,
-                2
-            ),
-            {
-                headers: HEADERS.json
-            }
-        )
+        return identity
     }
 }
