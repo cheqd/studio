f7f5b6885cd7524e6694d7c7154fccd3f79cf032
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-04-25 15:13:09 +0300

    Add cryptoBox with local KV storage
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>

--- credentials-back-end/src/bindings.d.ts
+++ credentials-back-end/src/bindings.d.ts
@@ -1,6 +1,7 @@
 export {}
 
 declare global {
+    const CREDENTIALS: KVNamespace
     // Env vars, secrets, KV bindings to be used
     // const ENV_VAR: string
     // const SECRET: string
--- credentials-back-end/src/error_handler.ts
+++ credentials-back-end/src/error_handler.ts
@@ -1,5 +1,6 @@
-import { ErrorResponse } from './api/types'
 
-const error_handler = ( error: ErrorResponse ): Response => { return new Response( error.message || 'Internal Server Error', { status: error.status || 500 } ) }
+const error_handler = ( request: Request ): Response => { 
+  return new Response('Not Found.', { status: 404 })
+}
 
 export default error_handler
