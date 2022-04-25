6a71fd1219492d47e1116ad968fd37cc01e6d192
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2022-04-25 20:09:53 +0300

    Fix CORS

--- credentials-back-end/src/handler.ts
+++ credentials-back-end/src/handler.ts
@@ -1,7 +1,35 @@
 import router from './router'
 
 export const handleRequest = async ( request: Request ): Promise<Response> => {
-    let response = await router.handle( request )
-    
+    // Handle CORS preflight request
+    if (
+        request.headers.get('Origin') !== null &&
+        request.headers.get('Access-Control-Request-Method') !== null
+    ) {
+        const origin = '*';
+        const methods = 'GET, POST, PATCH, DELETE';
+        const headers = 'referer, origin, content-type, authorization';
+
+        const corsHeaders = {
+            'Access-Control-Allow-Origin': origin,
+            'Access-Control-Allow-Methods': methods,
+            'Access-Control-Allow-Headers': headers,
+        }
+
+        // Handle CORS pre-flight request.
+        return new Response(null, {
+            status: 204,
+            headers: corsHeaders
+        })
+    }
+
+    // Handle request
+    const response = await router.handle( request )
+
+    // Inject CORS headers
+    response.headers.set("Access-Control-Allow-Origin", "*")
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    response.headers.set("Access-Control-Allow-Headers", "referer, origin, content-type, authorization")
+
     return response
 }
