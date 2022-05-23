d300881bbeefde940bf6d60247d8928a4f8cf4bd
Tasos <50984242+Eengineer1@users.noreply.github.com>
2022-05-23 16:12:38 +0300

    Cleaned up state, secrets

--- credentials-back-end/src/api/constants.ts
+++ credentials-back-end/src/api/constants.ts
@@ -311,19 +311,25 @@ agent:
             - $ref: /dbConnection
 `
 
-export const INFURA_PROJECT_ID = '***REMOVED***'
+export const INFURA_PROJECT_ID = _INFURA_PROJECT_ID
 
-export const KMS_SECRET_KEY = '***REMOVED***'
+export const KMS_SECRET_KEY = _KMS_SECRET_KEY
 
-export const ISSUER_ID = '***REMOVED***'
+export const ISSUER_ID = _ISSUER_ID
 
-export const ISSUER_ID_PRIVATE_KEY_HEX = '***REMOVED***'
+export const ISSUER_ID_PRIVATE_KEY_HEX = _ISSUER_ID_PRIVATE_KEY_HEX
+
+export const ISSUER_ID_PUBLIC_KEY_HEX = _ISSUER_ID_PUBLIC_KEY_HEX
+
+export const ISSUER_ID_KID = _ISSUER_ID_KID
+
+export const ISSUER_ID_METHOD_SPECIFIC_ID = _ISSUER_ID_METHOD_SPECIFIC_ID
+
+export const ISSUER_ID_METHOD = _ISSUER_ID_METHOD
 
 export const VC_CONTEXT = ['https://www.w3.org/2018/credentials/v1', 'https://veramo.io/contexts/profile/v1']
 
 export const VC_TYPE = 'VerifiableCredential'
-
-export const VC_SUBJECT = 'did:cheqd:testnet:DBC2ED07-7CFD-4A'
 
 export const VC_PROOF_FORMAT = 'jwt'
 
@@ -336,8 +342,8 @@ export const REPLY_PROTECTION_INTERVAL = 30
 
 // Twitter Auth
 
-export const TWITTER_CONSUMER_KEY = '***REMOVED***'
+export const TWITTER_CONSUMER_KEY = _TWITTER_CONSUMER_KEY
 
-export const TWITTER_CONSUMER_SECRET = '***REMOVED***'
+export const TWITTER_CONSUMER_SECRET = _TWITTER_CONSUMER_SECRET
 
-export const TWITTER_STATE_CSRF = '***REMOVED***'
+export const TWITTER_STATE_CSRF = _TWITTER_STATE_CSRF
