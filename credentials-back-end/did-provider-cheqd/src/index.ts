0fd8e491496a454756dc10e3cf11236c87c9e30d
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-05-31 13:15:15 +0100

    Moved everything one level up

--- credentials-back-end/did-provider-cheqd/src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @public
- */
-const schema = require('../plugin.schema.json')
-export { schema }
-export { CheqdDIDProvider } from './did-manager/cheqd-did-provider'
-export { CheqdDidResolver, getResolver } from './did-manager/cheqd-did-resolver'
-export { CheqdResolver } from './did-manager/resolver'
-export * from './types/IMyAgentPlugin'
