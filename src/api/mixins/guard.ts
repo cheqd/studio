0d50e060de2500e860cf32495d0b51f06c60775f
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2022-10-07 18:36:45 +0530

    wip --skip-ci

--- src/api/mixins/guard.ts
+++ src/api/mixins/guard.ts
@@ -3,32 +3,35 @@ import { Credentials } from '../controllers/credentials'
 import { GenericAuthResponse } from '../types'
 
 export class GuardedCredentials {
-    guard = async (request: Request): Promise<GenericAuthResponse> => {
-        const { claim, provider, subjectId } = await request.json() as { claim: string, provider: string, subjectId: string }
+	guard = async (request: Request): Promise<GenericAuthResponse> => {
+		const { claim, provider, subjectId } = await request.json() as { claim: string, provider: string, subjectId: string }
 
-        try {
-            const resp = await fetch(
-                AUTH0_SERVICE_ENDPOINT,
-                {
-                    method: 'POST',
-                    body: JSON.stringify(
-                        {
-                            claim,
-                            provider
-                        }
-                    ),
-                    headers: HEADERS.json
-                }
-            )
-            const validation = await resp.json()
-            return { ...validation as GenericAuthResponse, subjectId }
-        } catch (err) {
-            return { authenticated: false, user: null, error: err } as GenericAuthResponse
-        }
-        
-    }
+		console.log('method: guard - try with AUTH0_SERVICE_ENDPOINT')
+		try {
+			const resp = await fetch(
+				AUTH0_SERVICE_ENDPOINT,
+				{
+					method: 'POST',
+					body: JSON.stringify(
+						{
+							claim,
+							provider
+						}
+					),
+					headers: HEADERS.json
+				}
+			)
+			const validation = await resp.json()
+			console.log('validation successful')
+			return { ...validation as GenericAuthResponse, subjectId }
+		} catch (err) {
+			console.log('validation error: ', JSON.stringify(err))
+			return { authenticated: false, user: null, error: err } as GenericAuthResponse
+		}
+
+	}
 }
 
 export interface GuardedCredentials extends Credentials {
-    guard: (request: Request) => Promise<GenericAuthResponse>
+	guard: (request: Request) => Promise<GenericAuthResponse>
 }
