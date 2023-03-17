2199575ac1da8bfcfe3dfad4e1363fd2f6fd4aec
DaevMithran <daevmithran1999@gmail.com>
2023-03-17 18:21:02 +0530

    feat: Remove credential guard

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -4,9 +4,6 @@ export const HEADERS = {
 }
 
 export const VC_CONTEXT = ['https://www.w3.org/2018/credentials/v1', 'https://veramo.io/contexts/profile/v1']
-export const VC_PERSON_CONTEXT = ['https://schema.org/Person']
-export const VC_EVENTRESERVATION_CONTEXT = ['https://schema.org/EventReservation']
-export const VC_TICKET_CONTEXT = ['https://schema.org/Ticket']
 export const VC_TYPE: string = 'VerifiableCredential'
 export const VC_PROOF_FORMAT = 'jwt'
 export const VC_REMOVE_ORIGINAL_FIELDS = false
