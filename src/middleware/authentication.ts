67cfa78ca4fdcaca87c45ce8694a6d44bcc6c50e
DaevMithran <daevmithran1999@gmail.com>
2023-05-15 11:58:31 +0530

    feat: Minor fixes

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,5 +1,6 @@
 import { Response, NextFunction } from 'express'
 import { expressjwt, Request as JWTRequest } from 'express-jwt'
+import { CustomerService } from '../services/customer'
 
 require('dotenv').config()
 
@@ -21,12 +22,17 @@ export class Authentication {
         next()
     }
 
-    static authenticate(jwtRequest: JWTRequest, response: Response, next: NextFunction) {
+    static async authenticate(jwtRequest: JWTRequest, response: Response, next: NextFunction) {
         if(jwtRequest.path == '/') return next()
 
         if (!jwtRequest.auth?.sub) return response.status(401).json({
             error: 'Invalid auth token'
         })
+
+        if(jwtRequest.path != '/1.0/api/account' && !await CustomerService.instance.find(jwtRequest.auth.sub, {})) return response.status(401).json({
+            error: 'Customer not found'
+        })
+
         response.locals.customerId = jwtRequest.auth.sub
         next()
     }
