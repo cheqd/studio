7742b71a5eecc37c826bc9d3e9cd69043a04390e
DaevMithran <daevmithran1999@gmail.com>
2023-05-23 11:42:59 +0530

    fix: Minor merge issues

--- packages/frontend/src/lib/shared/types/rendering.types.ts
+++
@@ -1,5 +0,0 @@
-import type { CredentialListRenderSchema, CredentialRenderMap } from "$shared/schema";
-import type { z } from "zod";
-
-export type CredentialListRenderType = z.infer<typeof CredentialListRenderSchema>;
-export type CredentialRenderMapType = z.infer<typeof CredentialRenderMap>;
--- packages/frontend/src/lib/shared/types/server.types.ts
+++
@@ -1,6 +0,0 @@
-import type { z } from 'zod';
-import type {
-    UserOnboardingResponseSchema,
-} from '$shared/schema';
-
-export type UserOnboardingResponseType = z.infer<typeof UserOnboardingResponseSchema>;
