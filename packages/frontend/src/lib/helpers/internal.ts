c47494bc2021aafa7fcae9c4675932e1c1bd3de9
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-05-24 15:50:21 +0100

    Delete frontend

--- packages/frontend/src/lib/helpers/internal.ts
+++
@@ -1,5 +0,0 @@
-import type { Redirect } from '@sveltejs/kit';
-
-export const isSvelteKitRedirect = (val: any): val is Redirect => {
-	return val && val.status !== undefined && val.location !== undefined;
-};
