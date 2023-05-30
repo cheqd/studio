5c44288dd3726d5f38828126641d1476f9d0a897
DaevMithran <61043607+DaevMithran@users.noreply.github.com>
2023-05-30 00:50:57 -0700

    feat: Add auth toggle && missing api's (#234)
    * feat: Add create resource
    
    * feat: Add auth toggle

--- src/controllers/issuer.ts
+++ src/controllers/issuer.ts
@@ -2,14 +2,49 @@ import type { Request, Response } from 'express'
 
 import { DIDDocument } from 'did-resolver'
 import { v4 } from 'uuid'
-import { MethodSpecificIdAlgo } from '@cheqd/sdk'
+import { MethodSpecificIdAlgo, VerificationMethods, CheqdNetwork } from '@cheqd/sdk'
+import { MsgCreateResourcePayload } from '@cheqd/ts-proto/cheqd/resource/v2/index.js'
 
 import { Identity } from '../services/identity.js'
 import { CustomerService } from '../services/customer.js'
 import { generateDidDoc, validateSpecCompliantPayload } from '../helpers/helpers.js'
 import { CustomerEntity } from '../database/entities/customer.entity.js'
+import { check, param, validationResult } from 'express-validator'
+import { fromString } from 'uint8arrays'
 
 export class IssuerController {
+
+  public static didValidator = [
+    check('didDocument').optional().isArray().custom((value)=>{
+      const { valid } = validateSpecCompliantPayload(value)
+      return valid
+    }).withMessage('Invalid didDocument'),
+    check('secret.verificationMethod.type')
+      .optional()
+      .isString()
+      .isIn([VerificationMethods.Ed255192020, VerificationMethods.Ed255192018, VerificationMethods.JWK])
+      .withMessage('Invalid verificationMethod'),
+    check('secret.verificationMethod.id')
+      .optional()
+      .isString()
+      .withMessage('Invalid verificationMethod'),      
+    check('options.methodSpecificIdAlgo').optional().isString().isIn([MethodSpecificIdAlgo.Base58, MethodSpecificIdAlgo.Uuid]).withMessage('Invalid methodSpecificIdAlgo'),
+    check('options.network').optional().isString().isIn([CheqdNetwork.Mainnet, CheqdNetwork.Testnet]).withMessage('Invalid network'),
+  ]
+
+  public static resourceValidator = [
+    param('did').exists().isString().contains('did:cheqd').withMessage('Invalid DID'),
+    check('jobId').custom((value, {req})=>{
+      if(!value && !(req.body.name && req.body.type && req.body.data)) return false
+      return true
+    }).withMessage('name, type and data are required'),
+    check('name').optional().isString().withMessage('Invalid name'),
+    check('type').optional().isString().withMessage('Invalid type'),
+    check('data').optional().isString().withMessage('Invalid data'),
+    check('alsoKnownAs').optional().isArray().withMessage('Invalid alsoKnownAs'),
+    check('alsoKnownAs.*.uri').isString().withMessage('Invalid uri'),
+    check('alsoKnownAs.*.description').isString().withMessage('Invalid description')
+  ]
   
   public async createKey(request: Request, response: Response) {
     try {
@@ -39,13 +74,20 @@ export class IssuerController {
   }
 
   public async createDid(request: Request, response: Response) {
+    const result = validationResult(request)
+    if (!result.isEmpty()) {
+      return response.status(400).json({
+        error: result.array()[0].msg
+      })
+    }
+
     const { options, secret } = request.body
     const { methodSpecificIdAlgo, network, versionId = v4()} = options
     const verificationMethod = secret?.verificationMethod
     let didDocument: DIDDocument
     let kids: string[] = [] 
     try {
-      if (options.didDocument && validateSpecCompliantPayload(options.didDocument)) {
+      if (options.didDocument) {
         didDocument = options.didDocument
       } else if (verificationMethod) {
         const key = await Identity.instance.createKey()
@@ -71,6 +113,55 @@ export class IssuerController {
         return response.status(500).json({
             error: `${error}`
         })
+    }
+  }
+
+  public async createResource(request: Request, response: Response) {
+    const result = validationResult(request)
+    if (!result.isEmpty()) {
+      return response.status(400).json({
+        error: result.array()[0].msg
+      })
+    }
+
+    const { did } = request.params
+    let { data, name, type, alsoKnownAs, version } = request.body
+    
+    let resourcePayload: Partial<MsgCreateResourcePayload> = {}
+    try {
+      // check if did is registered on the ledger
+      let resolvedDocument = await Identity.instance.resolveDid(did)
+      if(!resolvedDocument?.didDocument || resolvedDocument.didDocumentMetadata.deactivated) {
+        return response.status(400).send({
+            error: `${did} is a Deactivated DID`
+        })
+      } else {
+        resolvedDocument = resolvedDocument.didDocument
+      }
+      
+      resourcePayload = {
+        collectionId: did.split(':').pop()!,
+        id: v4(),
+        name,
+        resourceType: type,
+        data: fromString(data, 'base64'),
+        version,
+        alsoKnownAs
+      }
+      const result = await Identity.instance.createResource(resourcePayload, response.locals.customerId)    
+      if ( result ) {
+        return response.status(201).json({
+            resource: resourcePayload
+        })
+      } else {
+        return response.status(500).json({
+            error: 'Error creating resource'
+        })
+      }
+    } catch (error) {
+      return response.status(500).json({
+        error: `${error}`
+      })
     }
   }
 
