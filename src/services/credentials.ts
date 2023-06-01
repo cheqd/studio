b97c97e1451c2e50080e4a653b502409590fd5bc
DaevMithran <daevmithran1999@gmail.com>
2023-06-01 14:28:20 +0530

    fix: Update Credential output to contain vc

--- src/services/credentials.ts
+++ src/services/credentials.ts
@@ -45,11 +45,11 @@ export class Credentials {
             }
         )
 
-		if (verifiable_credential?.vc) delete verifiable_credential.vc
-		if (verifiable_credential?.sub) delete verifiable_credential.sub
-		if (verifiable_credential?.iss) delete verifiable_credential.iss
-		if (verifiable_credential?.nbf) delete verifiable_credential.nbf
-		if (verifiable_credential?.exp) delete verifiable_credential.exp
+		// if (verifiable_credential?.vc) delete verifiable_credential.vc
+		// if (verifiable_credential?.sub) delete verifiable_credential.sub
+		// if (verifiable_credential?.iss) delete verifiable_credential.iss
+		// if (verifiable_credential?.nbf) delete verifiable_credential.nbf
+		// if (verifiable_credential?.exp) delete verifiable_credential.exp
         
         if (USE_VERIDA_CONNECTOR && request.subjectDid.startsWith('did:vda')) {
           await VeridaService.instance.sendCredential(
