8da6da42b131aadb83b3e5e5a81566f45696992a
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-06-07 19:59:37 +0500

    Add start and stop docker compose scripts.

---
+++ start-compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+USE_EXTERNAL_DB="false"
+
+if [[ "$USE_EXTERNAL_DB" == "true" ]]; then
+  docker compose build && docker compose up -d
+else
+  docker compose up -d credential-service
+fi
