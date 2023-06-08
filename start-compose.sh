6a2aed4c5a68d93d647f711b4718110916fc3797
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-06-08 16:28:26 +0500

    Remove start-compose and stop-compose scripts.

--- start-compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USE_EXTERNAL_DB="false"
-
-if [[ "$USE_EXTERNAL_DB" == "true" ]]; then
-  docker compose build && docker compose up -d
-else
-  docker compose up credential-service -d
-fi
