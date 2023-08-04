525357f3bd0888e0f89569497e587efd88f71e37
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-08-04 18:08:29 +0500

    Remove dynamically login/logout button.

--- src/static/custom-button.ts
+++ src/static/custom-button.ts
@@ -9,7 +9,6 @@ window.addEventListener("load", function () {
         window.location.href = base_url + '/logto/sign-in';
     };
     const logout_button = document.createElement('button');
-    logout_button.disabled = true;
     logout_button.innerHTML = 'Log out';
     logout_button.classList.add('btn', 'authorize');
     logout_button.onclick = function () {
@@ -18,21 +17,20 @@ window.addEventListener("load", function () {
     const auth_pan = document.createElement('div');
     auth_pan.classList.add('auth-wrapper');
     auth_pan.appendChild(login_button);
-    auth_pan.appendChild(logout_button);
 
     const scheme_pan = document.getElementsByClassName('scheme-container')[0];
     scheme_pan.children[0].appendChild(auth_pan);
 
-    fetchUserInfo(login_button, logout_button);
+    isAuthenticated().then(function(value) {
+        if (value) {
+            auth_pan.removeChild(login_button);
+            auth_pan.appendChild(logout_button);
+        }
+    });
 });
 
-async function fetchUserInfo(login_button: HTMLButtonElement, logout_button: HTMLButtonElement) {
+async function isAuthenticated() {
     const res = await fetch(`${window.location.origin}/auth/user-info`);
-    if (res.status === 200) { 
-        const body = await res.json();
-        if (body.isAuthenticated as boolean) {
-            login_button.disabled = true;
-            logout_button.disabled = false;
-        }
-    }
+    const body = await res.json();
+    return body.isAuthenticated as boolean;
 }
