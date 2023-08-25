fe9593065428006eca61c85a56d0a66040d838e1
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-08-25 11:58:06 +0500

    Update playwright.config.ts.

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:8787',
+    baseURL: 'http://localhost:8787',
 
     // Docs: https://playwright.dev/docs/api/class-testoptions#test-options-storage-state
 		storageState: '.state.json',
