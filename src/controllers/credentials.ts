436d7d205edba6f5d9b57656a07c63a2871f7e94
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-08-31 15:16:20 +0500

    Add implementation for blocking issue credential based on a deactivated DID.

--- src/controllers/credentials.ts
+++ src/controllers/credentials.ts
@@ -67,13 +67,6 @@ export class CredentialController {
 	 *     tags: [ Credential ]
 	 *     summary: Issue a Verifiable Credential
 	 *     description: This endpoint issues a Verifiable Credential. As input it takes the list of issuerDid, subjectDid, attributes, and other parameters of the credential to be issued.
-	 *     parameters:
-	 *       - in: query
-	 *         name: allowDeactivated
-	 *         description: If set to `true` allow issue credential using deactivated DID.
-	 *         schema:
-	 *           type: boolean
-	 *           default: false
 	 *     requestBody:
 	 *       content:
 	 *         application/x-www-form-urlencoded:
@@ -110,14 +103,11 @@ export class CredentialController {
 			request.body['@context'] = [request.body['@context']];
 		}
 
-		const allowDeactivated = request.query.allowDeactivated === "true";
-		if (!allowDeactivated) {
-			const resolvedResult = await new IdentityServiceStrategySetup(response.locals.customerId).agent.resolveDid(request.body.issuerDid);
-			if (!resolvedResult?.didDocument || resolvedResult.didDocumentMetadata.deactivated) {
-				return response.status(StatusCodes.BAD_REQUEST).send({
-					error: `${request.body.issuerDid} is either Deactivated or Not found`,
-				});
-			}
+		const resolvedResult = await new IdentityServiceStrategySetup(response.locals.customerId).agent.resolveDid(request.body.issuerDid);
+		if (!resolvedResult?.didDocument || resolvedResult.didDocumentMetadata.deactivated) {
+			return response.status(StatusCodes.BAD_REQUEST).send({
+				error: `${request.body.issuerDid} is either Deactivated or Not found`,
+			});
 		}
 
 		try {
