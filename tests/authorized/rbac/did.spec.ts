47cf1cbd061d2a11dbfd2d975a4e819019d0eda6
Andrew Nikitin <andrew.nikitin@cheqd.io>
2023-09-05 14:18:19 +0400

    Typo and remove unused code

--- tests/authorized/rbac/did.spec.ts
+++ tests/authorized/rbac/did.spec.ts
@@ -6,43 +6,7 @@ import * as fs from 'fs';
 
 const PAYLOADS_BASE_PATH="./tests/authorized/payloads/rbac/did";
 
-// Positive tests
-// test('Create DID for user with testnet role', async ({ DIDRepository }) => {
-//     expect(DIDRepository.did).not.toEqual('');
-// });
-
-// test('Search DID Document', async ({ DIDRepository }) => {
-//     expect(DIDRepository.did).not.toEqual({});
-// });
-
-// test('List of DID Documents', async ({ request, DIDRepository }) => {
-//     const did = DIDRepository.did;
-//     expect(DIDRepository.did).not.toEqual('');
-
-//     const response = await request.get(`/did/list`);
-//     expect(response.ok()).toBeTruthy();
-//     const didDocs = await response.json();
-//     // Expect that DIDTestnet is in list
-//     expect(didDocs).toEqual(expect.arrayContaining([did]));
-// });
-
-// test('Deactivate DIDDocument', async ({ DIDRepository, request }) => {
-//     const did = DIDRepository.did;
-//     expect(DIDRepository.did).not.toEqual('');;
-
-//     const response = await request.post(`/did/deactivate/${did}`);
-//     if (!response.ok()) {
-//         console.error(await response.text() + " For DID: " + did);
-//     }
-//     expect(response.ok()).toBeTruthy();
-
-//     const didResult = await DIDRepository.resolveDid(did);
-//     expect(didResult.didDocumentMetadata.deactivated).toBeTruthy();
-// });
-
-
-
-// Negative tests. All of this tests are should return 403 Forbidden 
+// Negative tests. All of this tests should return 403 Forbidden 
 // cause here the user tries to make mainnet operations with testnet role
 test('Create DID for user with testnet role but network is mainnet', async ({ request }) => {
     const response = await request.post(
