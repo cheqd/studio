2585e4209f78f9836136de40e6b884356d9e88bb
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-09-07 18:43:03 +0100

    Fix typo in DEFAULT_STATUS_LIST_INDICES

--- tests/constants.ts
+++ tests/constants.ts
@@ -6,7 +6,7 @@ export const DEFAULT_STATUS_LIST_ENCRYPTED_NAME = "cheqd-employee-credentials-en
 export const DEFAULT_STATUS_LIST_UNENCRYPTED_NAME = "cheqd-employee-credentials-unencrypted";
 export const DEFAULT_STATUS_LIST_PAYMENT_ADDRESS = "cheqd1qs0nhyk868c246defezhz5eymlt0dmajna2csg";
 export const DEFAULT_SYMMETRIC_KEY = "dfe204ee95ae74ea5d74b94c3d8ff782273905b07fbc9f8c3d961c3b43849f18";
-export const DEFAULT_STATUS_LIST_INDICIES = [10, 3199, 12109, 130999];
+export const DEFAULT_STATUS_LIST_INDICES = [10, 3199, 12109, 130999];
 
 // Credential names
 export const DEFAULT_SUBJECT_DID = DEFAULT_TESTNET_DID;
