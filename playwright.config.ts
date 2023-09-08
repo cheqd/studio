d16594095979f2fa97d9cb72045d151c11555135
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-09-08 16:39:46 +0100

    Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,16 +12,24 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
 
   testDir: './dist-tests',
+  
   /* Run tests in files in parallel */
   fullyParallel: true,
+  
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  
+  // Reporter to use for test results
+	// Uses GitHub Actions reporter on CI, otherwise uses HTML reporter
+	// Docs: https://playwright.dev/docs/test-reporters
+	reporter: process.env.CI ? 'github' : 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
