42fd3ed21f251bdab1d0d8a1f7436cd69ec65543
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-09-08 12:09:09 +0100

    Have only one playwright file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,10 @@
-import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-
-  // Global setup file for all tests
-	globalSetup: './tests/setup',
-
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -29,9 +20,6 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:8787',
-
-    // Docs: https://playwright.dev/docs/api/class-testoptions#test-options-storage-state
-		storageState: '.state.json',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
