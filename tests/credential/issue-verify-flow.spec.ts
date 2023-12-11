82106415f2e0dd2131e9af4703b12b73c3360bf5
DaevMithran <daevmithran1999@gmail.com>
2023-12-11 20:53:22 +0530

    Add assertionMethod

--- tests/credential/issue-verify-flow.spec.ts
+++ tests/credential/issue-verify-flow.spec.ts
@@ -9,11 +9,7 @@ const PAYLOADS_BASE_PATH = './tests/payloads/credential';
 let issuerDid: string;
 
 test(' Issue a jwt credential', async ({ request }) => {
-	const dids = (await (await request.get(`/did/list`)).json()) as string[];
-	issuerDid = dids.find((did) => did.startsWith('did:cheqd:testnet'));
-	console.log(`issuerDid: ${issuerDid}`);
 	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_BASE_PATH}/credential-issue-jwt.json`, 'utf-8'));
-	credentialData.issuerDid = issuerDid;
 	const response = await request.post(`/credential/issue`, {
 		data: JSON.stringify(credentialData),
 		headers: {
@@ -21,7 +17,6 @@ test(' Issue a jwt credential', async ({ request }) => {
 		},
 	});
 	const credential = await response.json();
-	console.log(credential);
 	expect(response).toBeOK();
 	expect(response.status()).toBe(StatusCodes.OK);
 	expect(credential.proof.type).toBe('JwtProof2020');
@@ -29,7 +24,6 @@ test(' Issue a jwt credential', async ({ request }) => {
 
 test(' Issue a jsonLD credential', async ({ request }) => {
 	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_BASE_PATH}/credential-issue-jsonld.json`, 'utf-8'));
-	credentialData.issuerDid = issuerDid;
 	const response = await request.post(`/credential/issue`, {
 		data: JSON.stringify(credentialData),
 		headers: {
@@ -37,7 +31,6 @@ test(' Issue a jsonLD credential', async ({ request }) => {
 		},
 	});
 	const credential = await response.json();
-	console.log(credential);
 	expect(response).toBeOK();
 	expect(response.status()).toBe(StatusCodes.OK);
 	expect(credential.proof.type).toBe('Ed25519Signature2018');
