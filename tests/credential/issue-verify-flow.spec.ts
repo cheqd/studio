81a14f8b2ff3ee3a73448b1f8a37548f3ce726d8
DaevMithran <daevmithran1999@gmail.com>
2023-12-12 13:35:48 +0530

    Add verify credential tests

--- tests/credential/issue-verify-flow.spec.ts
+++ tests/credential/issue-verify-flow.spec.ts
@@ -1,3 +1,5 @@
+import type { VerifiableCredential } from '@veramo/core';
+
 import { test, expect } from '@playwright/test';
 import { StatusCodes } from 'http-status-codes';
 import * as fs from 'fs';
@@ -6,7 +8,7 @@ test.use({ storageState: 'playwright/.auth/user.json' });
 
 const PAYLOADS_BASE_PATH = './tests/payloads/credential';
 
-let issuerDid: string;
+let jwtCredential: VerifiableCredential, jsonldCredential: VerifiableCredential;
 
 test(' Issue a jwt credential', async ({ request }) => {
 	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_BASE_PATH}/credential-issue-jwt.json`, 'utf-8'));
@@ -16,10 +18,37 @@ test(' Issue a jwt credential', async ({ request }) => {
 			'Content-Type': 'application/json',
 		},
 	});
-	const credential = await response.json();
+	jwtCredential = await response.json();
 	expect(response).toBeOK();
 	expect(response.status()).toBe(StatusCodes.OK);
-	expect(credential.proof.type).toBe('JwtProof2020');
+	expect(jwtCredential.proof.type).toBe('JwtProof2020');
+});
+
+test(' Verify a jwt credential', async ({ request }) => {
+	const response = await request.post(`/credential/verify`, {
+		data: JSON.stringify({
+			credential: jwtCredential.proof.jwt,
+		}),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	const result = await response.json();
+	expect(response).toBeOK();
+	expect(response.status()).toBe(StatusCodes.OK);
+	expect(result.verified).toBe(true);
+});
+
+test(' Issue a jwt credential with a deactivated DID', async ({ request }) => {
+	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_BASE_PATH}/credential-issue-jwt.json`, 'utf-8'));
+	credentialData.issuerDid = 'did:cheqd:testnet:edce6dfb-b59c-493b-a4b8-1d16a6184349';
+	const response = await request.post(`/credential/issue`, {
+		data: JSON.stringify(credentialData),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	expect(response.status()).toBe(StatusCodes.BAD_REQUEST);
 });
 
 test(' Issue a jsonLD credential', async ({ request }) => {
@@ -30,8 +59,25 @@ test(' Issue a jsonLD credential', async ({ request }) => {
 			'Content-Type': 'application/json',
 		},
 	});
-	const credential = await response.json();
+	jsonldCredential = await response.json();
 	expect(response).toBeOK();
 	expect(response.status()).toBe(StatusCodes.OK);
-	expect(credential.proof.type).toBe('Ed25519Signature2018');
+	expect(jsonldCredential.proof.type).toBe('Ed25519Signature2018');
+});
+
+test(' Verify a jsonld credential', async ({ request }) => {
+	const response = await request.post(`/credential/verify`, {
+		data: JSON.stringify({
+			credential: jsonldCredential,
+			fetchRemoteContexts: true,
+		}),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	const result = await response.json();
+	console.log(result);
+	expect(response).toBeOK();
+	expect(response.status()).toBe(StatusCodes.OK);
+	expect(result.verified).toBe(true);
 });
