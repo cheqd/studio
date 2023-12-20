47e28e0504f4bb07188c4a22c75a70c8147572fb
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-12-20 16:29:53 +0100

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,19 +1,19 @@
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
 	moduleNameMapper: {
-        "^(\\.{1,2}/.*)\\.js$": "$1",
-      },
-      extensionsToTreatAsEsm: [".ts"],
-    transform: {
-    "^.+\\.(mt|t|cj|j)s$": [
-        "ts-jest",
-        {
-            useESM: true
-        }
-    ]
-    },
-	testRegex: "/test/.*\\.test\\.ts$",
-	collectCoverageFrom: ["src/**/*.{ts,js}"],
-	moduleDirectories: ["node_modules", 'src'],
-    testEnvironment: 'node',
+		'^(\\.{1,2}/.*)\\.js$': '$1',
+	},
+	extensionsToTreatAsEsm: ['.ts'],
+	transform: {
+		'^.+\\.(mt|t|cj|j)s$': [
+			'ts-jest',
+			{
+				useESM: true,
+			},
+		],
+	},
+	testRegex: '/test/.*\\.test\\.ts$',
+	collectCoverageFrom: ['src/**/*.{ts,js}'],
+	moduleDirectories: ['node_modules', 'src'],
+	testEnvironment: 'node',
 };
