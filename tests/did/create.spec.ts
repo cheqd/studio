307fd675f4aa905d007e3e12037a5c8f1ecda0db
abdulla-ashurov <99400260+abdulla-ashurov@users.noreply.github.com>
2023-12-20 16:32:31 +0500

    test: Credential-Service: Add integration tests for unauthorized scenario [DEV-3141] (#364)
    * Add playwright.config.ts.
    
    * Add setup script for authorizing before running
    tests.
    
    * Add playwright reports to .gitignore.
    
    * Update package-lock.json.
    
    * Add a new script for running e2e tests.
    
    * Update tsconfig.json.
    
    * Add playwright.yml for GitHub Actions.
    
    * Remove unused includes in tsconfig.json.
    
    * Add new test user environment variables.
    
    * Update playwright.config.ts.
    
    * Use env variables instead of magic strings.
    
    * Add test example.
    
    * Update playwright.yml.
    
    * Add common e2e tests for an unauthorized scenario.
    
    * Update package-lock.json.
    
    * Update package-lock.json.
    
    * Update playwright-ct.config.ts.
    
    * Add different commands for running authorized and
    unauthorized integration tests.
    
    * Add positive integration tests payloads.
    
    * Add more positive integration tests.
    
    * Remove an unused import.
    
    * Remove an incorrect line in .gitignore.
    
    * Update integration tests and payloads.
    
    * Add negative integration tests for:
    - /did/search;
    - /resource/search.
    
    * Update integration tests' description.
    
    * Add negative integration tests for
    /credential/verify endpoint.
    
    * Add negative integration tests for
    /status-list/search endpoint.
    
    * Add negative integration tests for
    /credential-status/check endpoint.
    
    * Add TODO comment for completing e2e tests.
    
    * Refactor integration tests.
    
    * Move constants.ts to inside "tests" folder.
    
    * Remove test user from example.env
    
    * Delete playwright.yml
    
    * Update build.yml
    
    * Add condition for checks
    
    * Update .gitignore
    
    * Fix test script command
    
    * Have only one playwright file
    
    * Update environment.d.ts
    
    * Update constants.ts.
    
    * Remove verida files from .gitignore so it blocks
    integration tests.
    
    * Re-architecture integration tests.
    
    * Add project to running separately no-auth.tests.
    
    * Use constant global variable instead of magic str.
    
    * Refactor integration tests' constants.
    
    * Refactor integration tests.
    
    * Update playwright.config.ts
    
    * Fix an error playwright.config.ts.
    
    * Refactor tests
    
    * Get rid of dupliated constants
    
    * Fix tests due ti latest changes
    
    * Update mlc_config.json
    
    * Actions updates
    
    * lockfile
    
    ---------
    
    Co-authored-by: Ankur Banerjee <ankurdotb@users.noreply.github.com>
    Co-authored-by: Andrew Nikitin <andrew.nikitin@cheqd.io>

---
+++ tests/did/create.spec.ts
@@ -0,0 +1,21 @@
+import {
+	CONTENT_TYPE,
+	PAYLOADS_PATH,
+	STORAGE_STATE_AUTHENTICATED,
+	DEFAULT_DOES_NOT_HAVE_PERMISSIONS
+} from '../constants';
+import * as fs from 'fs';
+import { test, expect } from '@playwright/test';
+import { StatusCodes } from 'http-status-codes';
+
+test.use({ storageState: STORAGE_STATE_AUTHENTICATED });
+
+test('[Negative] It cannot create DID in mainnet network for user with testnet role', async ({ request }) => {
+	const response = await request.post(`/did/create`, {
+		data: JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.DID}/did-create-without-permissions.json`, 'utf-8')),
+		headers: { 'Content-Type': CONTENT_TYPE.APPLICATION_JSON }
+	});
+	expect(response).not.toBeOK();
+	expect(response.status()).toBe(StatusCodes.FORBIDDEN);
+	expect(await response.text()).toEqual(expect.stringContaining(DEFAULT_DOES_NOT_HAVE_PERMISSIONS));
+});
---
+++ tests/did/update.spec.ts
@@ -0,0 +1,21 @@
+import {
+	CONTENT_TYPE,
+	PAYLOADS_PATH,
+	STORAGE_STATE_AUTHENTICATED,
+	DEFAULT_DOES_NOT_HAVE_PERMISSIONS
+} from '../constants';
+import * as fs from 'fs';
+import { test, expect } from '@playwright/test';
+import { StatusCodes } from 'http-status-codes';
+
+test.use({ storageState: STORAGE_STATE_AUTHENTICATED });
+
+test('[Negative] It cannot update DID in mainnet network for user with testnet role', async ({ request }) => {
+	const response = await request.post(`/did/update`, {
+		data: JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.DID}/did-update-without-permissions.json`, 'utf-8')),
+		headers: { 'Content-Type': CONTENT_TYPE.APPLICATION_JSON }
+	});
+	expect(response).not.toBeOK();
+	expect(response.status()).toBe(StatusCodes.FORBIDDEN);
+	expect(await response.text()).toEqual(expect.stringContaining(DEFAULT_DOES_NOT_HAVE_PERMISSIONS));
+});
