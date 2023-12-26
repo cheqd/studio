a20e80d118cc9478d633a601a64f8e77b726aa37
Andrew Nikitin <andrew.nikitin@cheqd.io>
2023-12-26 18:24:43 +0100

    fix: Move payloads to e2e folder (#452)
    * Move payloads to e2e folder
    
    * Fix paths to payloads in tests
    
    * Fix CHANGELOG
    
    * Fix CHANGELOG

--- tests/e2e/credential/issue-verify-flow.spec.ts
+++ tests/e2e/credential/issue-verify-flow.spec.ts
@@ -3,16 +3,14 @@ import type { VerifiableCredential } from '@veramo/core';
 import { test, expect } from '@playwright/test';
 import { StatusCodes } from 'http-status-codes';
 import * as fs from 'fs';
-import { CONTENT_TYPE } from '../constants';
+import { CONTENT_TYPE, PAYLOADS_PATH } from '../constants';
 
 test.use({ storageState: 'playwright/.auth/user.json' });
-
-const PAYLOADS_BASE_PATH = './tests/payloads/credential';
 
 let jwtCredential: VerifiableCredential, jsonldCredential: VerifiableCredential;
 
 test(' Issue a jwt credential', async ({ request }) => {
-	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_BASE_PATH}/credential-issue-jwt.json`, 'utf-8'));
+	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.CREDENTIAL}/credential-issue-jwt.json`, 'utf-8'));
 	const response = await request.post(`/credential/issue`, {
 		data: JSON.stringify(credentialData),
 		headers: {
@@ -50,7 +48,7 @@ test(' Verify a jwt credential', async ({ request }) => {
 });
 
 test(' Issue a jwt credential with a deactivated DID', async ({ request }) => {
-	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_BASE_PATH}/credential-issue-jwt.json`, 'utf-8'));
+	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.CREDENTIAL}/credential-issue-jwt.json`, 'utf-8'));
 	credentialData.issuerDid = 'did:cheqd:testnet:edce6dfb-b59c-493b-a4b8-1d16a6184349';
 	const response = await request.post(`/credential/issue`, {
 		data: JSON.stringify(credentialData),
@@ -62,7 +60,7 @@ test(' Issue a jwt credential with a deactivated DID', async ({ request }) => {
 });
 
 test(' Issue a jsonLD credential', async ({ request }) => {
-	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_BASE_PATH}/credential-issue-jsonld.json`, 'utf-8'));
+	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.CREDENTIAL}/credential-issue-jsonld.json`, 'utf-8'));
 	const response = await request.post(`/credential/issue`, {
 		data: JSON.stringify(credentialData),
 		headers: {
