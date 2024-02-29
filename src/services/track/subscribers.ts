6b50ee81ada4e4d06a73c5db57df38c18855ff63
Andrew Nikitin <andrew.nikitin@cheqd.io>
2024-02-29 18:26:28 +0100

    fix: Get rid of tracking data corruption (#496)
    * Add resource tracker and notifier
    
    * Fix building and format
    
    * Add tracking framework
    
    * Small changes
    
    * Fix build
    
    * Fix review comments
    
    * feat: Add tracking for failed client responses [DEV-3629] (#490)
    
    Add tracking for failed client responses
    
    * Add tracking payments
    
    * Fix defaultFee setting
    
    * Move defaultFee, fe and amount columns to CoinEntity and add Coin table
    
    * Fix tracking data corruption
    
    ---------
    
    Co-authored-by: Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>

--- src/services/track/subscribers.ts
+++ src/services/track/subscribers.ts
@@ -200,10 +200,11 @@ export class ResourceSubscriber extends BaseOperationObserver implements IObserv
 			// Just skip this operation
 			return;
 		}
-		trackOperation.category = OperationCategoryNameEnum.RESOURCE;
-		trackOperation.name = OperationNameEnum.RESOURCE_CREATE;
+		const resourceOperation = {...trackOperation};
+		resourceOperation.category = OperationCategoryNameEnum.RESOURCE;
+		resourceOperation.name = OperationNameEnum.RESOURCE_CREATE;
 		// tracking resource creation in DB
-		const result = await this.trackResourceOperation(trackOperation);
+		const result = await this.trackResourceOperation(resourceOperation);
 		// notify about the result of tracking, e.g. log or datadog
 		await this.notify({
 			message: this.compileMessage(result),
