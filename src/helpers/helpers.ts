b9a1142f9278a37cdfbd5543e752f57ad3f6d6d3
Andrew Nikitin <andrew.nikitin@cheqd.io>
2024-04-08 10:11:19 +0200

    build: Move to 4.x.y family for did-provider-cheqd and sdk [DEV-3799] (#509)
    * Move to 4.x.y family for did-provider-cheqd and sdk
    
    * Move to fixed did-provider-cheqd package version
    
    * Bump did-provider-cheqd package
    
    * Bump other packages
    
    * Bump timeout for auth test
    
    * Run tests in workflow
    
    * Fix litnetwork type
    
    * Downgrade playwright
    
    * fix  verida test
    
    * fix presentation tests
    
    * Comment out payment presentation test
    
    * Bump latest
    
    ---------
    
    Co-authored-by: DaevMithran <61043607+DaevMithran@users.noreply.github.com>
    Co-authored-by: DaevMithran <daevmithran1999@gmail.com>
    Co-authored-by: Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -60,13 +60,13 @@ export function toDefaultDkg(did: string): DkgOptions {
 		case CheqdNetwork.Mainnet:
 			return {
 				chain: LitCompatibleCosmosChains.cheqdMainnet,
-				network: LitNetworks.serrano,
+				network: LitNetworks.cayenne,
 			};
 		case CheqdNetwork.Testnet:
 		default:
 			return {
 				chain: LitCompatibleCosmosChains.cheqdTestnet,
-				network: LitNetworks.serrano,
+				network: LitNetworks.cayenne,
 			};
 	}
 }
@@ -108,7 +108,7 @@ export function isValidService(didDocument: DIDDocument): boolean {
 	return didDocument.service
 		? didDocument?.service?.every((s) => {
 				return s?.serviceEndpoint && s?.id && s?.type;
-		  })
+			})
 		: true;
 }
 
