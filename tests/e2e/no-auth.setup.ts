35aa8beb6cd1ad4deddc7ae591fe3321d1ddc6fe
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2024-07-04 11:21:17 +0300

    revert changes on no-auth.setup.ts

--- tests/e2e/no-auth.setup.ts
+++ tests/e2e/no-auth.setup.ts
@@ -12,11 +12,9 @@ setup('Unauthenticated user', async ({ page, request }) => {
 	const authorizeButton = await page.locator('button.btn.authorize.unlocked:has(span:has-text("Authorize"))');
 	await expect(authorizeButton).toBeVisible();
 
-	// commenting this out, as we're intercepting and appending api-key to all requests.
-
-	// const response = await page.goto(`${process.env.APPLICATION_BASE_URL}/account`);
-	// expect(response.ok()).not.toBe(true);
-	// expect(response.status()).toBe(StatusCodes.UNAUTHORIZED);
+	const response = await page.goto(`${process.env.APPLICATION_BASE_URL}/account`);
+	expect(response.ok()).not.toBe(true);
+	expect(response.status()).toBe(StatusCodes.UNAUTHORIZED);
 
 	// End of authentication steps.
 
