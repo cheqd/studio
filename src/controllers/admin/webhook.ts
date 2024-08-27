7dd0508dccf0d73eb74034e84874b88af8a30c88
DaevMithran <daevmithran1999@gmail.com>
2024-08-27 20:17:31 +0530

    Add error handler for webhooks

--- src/controllers/admin/webhook.ts
+++ src/controllers/admin/webhook.ts
@@ -43,69 +43,91 @@ export class WebhookController {
 			} satisfies INotifyMessage);
 			return response.sendStatus(StatusCodes.BAD_REQUEST);
 		}
-		// Handle the event
-		switch (event.type) {
-			case 'customer.subscription.trial_will_end':
-				subscription = event.data.object;
-				status = subscription.status;
-				await eventTracker.notify({
-					message: EventTracker.compileBasicNotification(
-						`Subscription status is ${status} for subscription with id: ${subscription.id}`,
-						'Stripe Webhook: customer.subscription.trial_will_end'
-					),
-					severity: 'info',
-				} satisfies INotifyMessage);
-				await eventTracker.submit(
-					buildSubmitOperation(subscription, OperationNameEnum.SUBSCRIPTION_TRIAL_WILL_END)
-				);
-				break;
-			case 'customer.subscription.deleted':
-				subscription = event.data.object;
-				status = subscription.status;
-				await eventTracker.notify({
-					message: EventTracker.compileBasicNotification(
-						`Subscription status is ${status} for subscription with id: ${subscription.id}`,
-						'Stripe Webhook: customer.subscription.deleted'
-					),
-					severity: 'info',
-				} satisfies INotifyMessage);
-				await eventTracker.submit(buildSubmitOperation(subscription, OperationNameEnum.SUBSCRIPTION_CANCEL));
-				break;
-			case 'customer.subscription.created':
-				subscription = event.data.object;
-				status = subscription.status;
-				await eventTracker.notify({
-					message: EventTracker.compileBasicNotification(
-						`Subscription status is ${status} for subscription with id: ${subscription.id}`,
-						'Stripe Webhook: customer.subscription.created'
-					),
-					severity: 'info',
-				} satisfies INotifyMessage);
-				await eventTracker.submit(buildSubmitOperation(subscription, OperationNameEnum.SUBSCRIPTION_CREATE));
-				break;
-			case 'customer.subscription.updated':
-				subscription = event.data.object;
-				status = subscription.status;
-				await eventTracker.notify({
-					message: EventTracker.compileBasicNotification(
-						`Subscription status is ${status} for subscription with id: ${subscription.id}`,
-						'Stripe Webhook: customer.subscription.updated'
-					),
-					severity: 'info',
-				} satisfies INotifyMessage);
-				await eventTracker.submit(buildSubmitOperation(subscription, OperationNameEnum.SUBSCRIPTION_UPDATE));
-				break;
-			default:
-				// Unexpected event type
-				await eventTracker.notify({
-					message: EventTracker.compileBasicNotification(
-						`Unexpected event: ${event} with type: ${event?.type}`,
-						'Stripe Webhook: unexpected'
-					),
-					severity: 'error',
-				} satisfies INotifyMessage);
+
+		try {
+			// Handle the event
+			switch (event.type) {
+				case 'customer.subscription.trial_will_end':
+					subscription = event.data.object;
+					status = subscription.status;
+					await eventTracker.notify({
+						message: EventTracker.compileBasicNotification(
+							`Subscription status is ${status} for subscription with id: ${subscription.id}`,
+							'Stripe Webhook: customer.subscription.trial_will_end'
+						),
+						severity: 'info',
+					} satisfies INotifyMessage);
+					await eventTracker.submit(
+						buildSubmitOperation(subscription, OperationNameEnum.SUBSCRIPTION_TRIAL_WILL_END)
+					);
+					break;
+				case 'customer.subscription.deleted':
+					subscription = event.data.object;
+					status = subscription.status;
+					await eventTracker.notify({
+						message: EventTracker.compileBasicNotification(
+							`Subscription status is ${status} for subscription with id: ${subscription.id}`,
+							'Stripe Webhook: customer.subscription.deleted'
+						),
+						severity: 'info',
+					} satisfies INotifyMessage);
+					await eventTracker.submit(
+						buildSubmitOperation(subscription, OperationNameEnum.SUBSCRIPTION_CANCEL)
+					);
+					break;
+				case 'customer.subscription.created':
+					subscription = event.data.object;
+					status = subscription.status;
+					await eventTracker.notify({
+						message: EventTracker.compileBasicNotification(
+							`Subscription status is ${status} for subscription with id: ${subscription.id}`,
+							'Stripe Webhook: customer.subscription.created'
+						),
+						severity: 'info',
+					} satisfies INotifyMessage);
+					await eventTracker.submit(
+						buildSubmitOperation(subscription, OperationNameEnum.SUBSCRIPTION_CREATE)
+					);
+					break;
+				case 'customer.subscription.updated':
+					subscription = event.data.object;
+					status = subscription.status;
+					await eventTracker.notify({
+						message: EventTracker.compileBasicNotification(
+							`Subscription status is ${status} for subscription with id: ${subscription.id}`,
+							'Stripe Webhook: customer.subscription.updated'
+						),
+						severity: 'info',
+					} satisfies INotifyMessage);
+					await eventTracker.submit(
+						buildSubmitOperation(subscription, OperationNameEnum.SUBSCRIPTION_UPDATE)
+					);
+					break;
+				default:
+					// Unexpected event type
+					await eventTracker.notify({
+						message: EventTracker.compileBasicNotification(
+							`Unexpected event: ${event} with type: ${event?.type}`,
+							'Stripe Webhook: unexpected'
+						),
+						severity: 'error',
+					} satisfies INotifyMessage);
+			}
+			// Return a 200 response to acknowledge receipt of the event
+			return response.status(StatusCodes.OK).send();
+		} catch (error) {
+			// Unexpected event type
+			await eventTracker.notify({
+				message: EventTracker.compileBasicNotification(
+					`Webhook failed: ${event} with type: ${event?.type}`,
+					'Stripe Webhook: unexpected'
+				),
+				severity: 'error',
+			} satisfies INotifyMessage);
+
+			return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+				error: `Internal error: ${(error as Error)?.message || error}`,
+			});
 		}
-		// Return a 200 response to acknowledge receipt of the event
-		return response.status(StatusCodes.OK).send();
 	}
 }
