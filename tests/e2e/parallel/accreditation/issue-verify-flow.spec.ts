022639b65931a2f97e763462cc49582084d56010
DaevMithran <daevmithran1999@gmail.com>
2024-09-30 12:12:03 +0530

    Add accredit & attest tests

--- tests/e2e/parallel/accreditation/issue-verify-flow.spec.ts
+++ tests/e2e/parallel/accreditation/issue-verify-flow.spec.ts
@@ -5,10 +5,8 @@ import { CONTENT_TYPE, PAYLOADS_PATH } from '../../constants';
 
 test.use({ storageState: 'playwright/.auth/user.json' });
 
-test(' Issue and verify a accreditation', async ({ request }) => {
-	const credentialData = JSON.parse(
-		fs.readFileSync(`${PAYLOADS_PATH.ACCREDITATION}/accreditation-issue-jwt.json`, 'utf-8')
-	);
+test(' Issue and verify a authorize accreditation', async ({ request }) => {
+	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.ACCREDITATION}/authorize-jwt.json`, 'utf-8'));
 	const issueResponse = await request.post(`/accreditation/issue?accreditationType=authorize`, {
 		data: JSON.stringify(credentialData),
 		headers: {
@@ -50,9 +48,7 @@ test(' Issue and verify a accreditation', async ({ request }) => {
 });
 
 test(' Issue accreditation [Negative]', async ({ request }) => {
-	const credentialData = JSON.parse(
-		fs.readFileSync(`${PAYLOADS_PATH.ACCREDITATION}/accreditation-issue-jwt.json`, 'utf-8')
-	);
+	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.ACCREDITATION}/accredit.json`, 'utf-8'));
 	const issueResponse = await request.post(`/accreditation/issue`, {
 		data: JSON.stringify(credentialData),
 		headers: {
@@ -69,4 +65,88 @@ test(' Verify accreditation [Negative]', async ({ request }) => {
 		},
 	});
 	expect(verifyResponse.status()).toBe(400);
+});
+
+test(' Issue and verify a accredit accreditation', async ({ request }) => {
+	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.ACCREDITATION}/accredit-jwt.json`, 'utf-8'));
+	const issueResponse = await request.post(`/accreditation/issue?accreditationType=authorize`, {
+		data: JSON.stringify(credentialData),
+		headers: {
+			'Content-Type': CONTENT_TYPE.APPLICATION_JSON,
+		},
+	});
+	const { didUrls, accreditation } = await issueResponse.json();
+	expect(issueResponse).toBeOK();
+	expect(issueResponse.status()).toBe(StatusCodes.OK);
+	expect(accreditation.proof.type).toBe('JwtProof2020');
+	expect(accreditation.proof).toHaveProperty('jwt');
+	expect(typeof accreditation.issuer === 'string' ? accreditation.issuer : accreditation.issuer.id).toBe(
+		credentialData.issuerDid
+	);
+	expect(accreditation.type).toContain('VerifiableCredential');
+	expect(accreditation.credentialSubject).toMatchObject({
+		...credentialData.attributes,
+		id: credentialData.subjectDid,
+	});
+	expect(didUrls).toHaveLength(2);
+	expect(didUrls).toContain(
+		`did:cheqd:testnet:15b74787-6e48-4fd5-8020-eab24e990578?resourceName=accreditAccreditation&resourceType=VerifiableAccreditationToAccredit`
+	);
+
+	const verifyResponse = await request.post(`/accreditation/verify`, {
+		data: JSON.stringify({
+			subjectDid: `${credentialData.subjectDid}`,
+			accreditation: `did:cheqd:testnet:15b74787-6e48-4fd5-8020-eab24e990578?resourceName=accreditAccreditation&resourceType=VerifiableAccreditationToAccredit`,
+		}),
+		headers: {
+			'Content-Type': CONTENT_TYPE.APPLICATION_JSON,
+		},
+	});
+	const result = await verifyResponse.json();
+	expect(verifyResponse).toBeOK();
+	expect(verifyResponse.status()).toBe(StatusCodes.OK);
+	expect(result.error).toBe(undefined);
+	expect(result.verified).toBe(true);
+});
+
+test(' Issue and verify a attest accreditation', async ({ request }) => {
+	const credentialData = JSON.parse(fs.readFileSync(`${PAYLOADS_PATH.ACCREDITATION}/accredit-jwt.json`, 'utf-8'));
+	const issueResponse = await request.post(`/accreditation/issue?accreditationType=authorize`, {
+		data: JSON.stringify(credentialData),
+		headers: {
+			'Content-Type': CONTENT_TYPE.APPLICATION_JSON,
+		},
+	});
+	const { didUrls, accreditation } = await issueResponse.json();
+	expect(issueResponse).toBeOK();
+	expect(issueResponse.status()).toBe(StatusCodes.OK);
+	expect(accreditation.proof.type).toBe('JwtProof2020');
+	expect(accreditation.proof).toHaveProperty('jwt');
+	expect(typeof accreditation.issuer === 'string' ? accreditation.issuer : accreditation.issuer.id).toBe(
+		credentialData.issuerDid
+	);
+	expect(accreditation.type).toContain('VerifiableCredential');
+	expect(accreditation.credentialSubject).toMatchObject({
+		...credentialData.attributes,
+		id: credentialData.subjectDid,
+	});
+	expect(didUrls).toHaveLength(2);
+	expect(didUrls).toContain(
+		`did:cheqd:testnet:BjS4Nv8bVdxm2WW28MCfXA?resourceName=attestAccreditation&resourceType=VerifiableAccreditationToAttest`
+	);
+
+	const verifyResponse = await request.post(`/accreditation/verify`, {
+		data: JSON.stringify({
+			subjectDid: `${credentialData.subjectDid}`,
+			accreditation: `did:cheqd:testnet:BjS4Nv8bVdxm2WW28MCfXA?resourceName=attestAccreditation&resourceType=VerifiableAccreditationToAttest`,
+		}),
+		headers: {
+			'Content-Type': CONTENT_TYPE.APPLICATION_JSON,
+		},
+	});
+	const result = await verifyResponse.json();
+	expect(verifyResponse).toBeOK();
+	expect(verifyResponse.status()).toBe(StatusCodes.OK);
+	expect(result.error).toBe(undefined);
+	expect(result.verified).toBe(true);
 });
