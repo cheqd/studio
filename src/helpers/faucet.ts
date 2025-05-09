8b660221c3987c54667654b7d8a44c1e3653d1a5
DaevMithran <61043607+DaevMithran@users.noreply.github.com>
2025-05-09 11:22:09 +0530

    fix: Fix faucet request (#675)
    * fix: Fix faucet request
    
    * Additional params to faucet request body
    
    * npm run format
    
    * fix: Join key in bootstrap
    
    ---------
    
    Co-authored-by: filipdjokic <djokicf@protonmail.com>

--- src/helpers/faucet.ts
+++ src/helpers/faucet.ts
@@ -1,17 +1,22 @@
 import type { ICommonErrorResponse } from '../types/authentication.js';
-import { MINIMAL_DENOM, FAUCET_URI } from '../types/constants.js';
+import { MINIMAL_DENOM, FAUCET_URI, FAUCET_AMOUNT } from '../types/constants.js';
 
 export class FaucetHelper {
 	// ...
-	static async delegateTokens(address: string): Promise<ICommonErrorResponse> {
+	static async delegateTokens(address: string, name: string, email: string): Promise<ICommonErrorResponse> {
 		const faucetURI = FAUCET_URI;
 		const faucetBody = {
 			denom: MINIMAL_DENOM,
 			address: address,
+			email,
+			name,
+			amount: FAUCET_AMOUNT,
+			marketingOptIn: false,
 		};
 		const response = await fetch(faucetURI, {
 			headers: {
 				'Content-Type': 'application/json',
+				'CF-IPCountry': 'CS', // To differentiate users coming from cheqd Studio
 			},
 			body: JSON.stringify(faucetBody),
 			method: 'POST',
