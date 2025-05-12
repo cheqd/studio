0a63695200a63a685ba50965ae7a08b8f48c2f55
filipdjokic <djokicf@protonmail.com>
2025-05-12 13:22:08 +0200

    Update migration script && add migrations workflow

--- src/database/migrations/InsertFingerprintApiKeyTable.ts
+++ src/database/migrations/InsertFingerprintApiKeyTable.ts
@@ -7,13 +7,13 @@ export class InsertFingerprintAPIKeyTable1746780465032 implements MigrationInter
 		const tableName = 'apiKey';
 		const secretBox = new SecretBox(process.env.EXTERNAL_DB_ENCRYPTION_KEY);
 
-		// Step 1: Add the fingerprint column (non-nullable, no default)
+		// Step 1: Add the fingerprint column (nullable, no default)
 		await queryRunner.addColumn(
 			tableName,
 			new TableColumn({
 				name: 'fingerprint',
 				type: 'varchar',
-				isNullable: false,
+				isNullable: true,
 			})
 		);
 
@@ -28,7 +28,18 @@ export class InsertFingerprintAPIKeyTable1746780465032 implements MigrationInter
 			]);
 		}
 
-		// Step 3: Add a unique index on fingerprint
+		// Step 3: Make the fingerprint column non-nullable (to allow unique index)
+		await queryRunner.changeColumn(
+			tableName,
+			'fingerprint',
+			new TableColumn({
+				name: 'fingerprint',
+				type: 'varchar',
+				isNullable: false,
+			})
+		);
+
+		// Step 4: Add a unique index on fingerprint
 		await queryRunner.query(
 			`CREATE UNIQUE INDEX "IDX_apiKey_c5fdf6760b38094e0905ac85e4" ON "${tableName}" ("fingerprint") `
 		);
