8ff60b9a46e7be925ba7e746681c3ebb5ea7866f
Sownak Roy <sownak@cheqd.io>
2025-06-06 14:05:49 +0100

    Revert "updated testcase"
    This reverts commit 42c0b18c9f403cbc7a7fd1cd1105f6890258f28a.

--- tests/e2e/parallel/did/create-negative.spec.ts
+++ tests/e2e/parallel/did/create-negative.spec.ts
@@ -83,7 +83,7 @@ test('[Negative] It cannot create DID with an invalid VerificationMethodType in 
 		},
 		headers: { 'Content-Type': CONTENT_TYPE.APPLICATION_JSON },
 	});
-	expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+	expect(response.status()).toBe(StatusCodes.BAD_REQUEST);
 	expect(await response.text()).toEqual(expect.stringContaining('Unsupported verificationMethod type'));
 });
 
