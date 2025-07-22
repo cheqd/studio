65df21cb920e383045d04372b5f2efa44a312b68
filipdjokic <djokicf@protonmail.com>
2025-07-22 10:11:06 +0200

    ci: Remove staging-specific DB changes

--- src/database/migrations/1750427001486-studio-migrations.ts
+++ src/database/migrations/1750427001486-studio-migrations.ts
@@ -4,7 +4,7 @@ export class StudioMigrations1750427001486 implements MigrationInterface {
     name = 'StudioMigrations1750427001486'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "subscription" DROP CONSTRAINT "FK_4fb9a7c3c5b5fecf58989794dcc"`);
+        
         await queryRunner.query(`ALTER TABLE "role" ALTER COLUMN "logToRoleIds" SET DEFAULT '{}'`);
         await queryRunner.query(`ALTER TABLE "payment" ALTER COLUMN "resourceId" DROP NOT NULL`);
     }
@@ -12,7 +12,6 @@ export class StudioMigrations1750427001486 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "payment" ALTER COLUMN "resourceId" SET NOT NULL`);
         await queryRunner.query(`ALTER TABLE "role" ALTER COLUMN "logToRoleIds" SET DEFAULT ARRAY[]`);
-        await queryRunner.query(`ALTER TABLE "subscription" ADD CONSTRAINT "FK_4fb9a7c3c5b5fecf58989794dcc" FOREIGN KEY ("customerId") REFERENCES "customer"("customerId") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
 }
